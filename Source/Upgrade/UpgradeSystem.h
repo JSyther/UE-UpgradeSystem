44b56a9fb59e30fe11b81d78f136e9ac9e5feca7
JSyther <108347205+JSyther@users.noreply.github.com>
2025-07-26 09:41:29 +0300

    Create UpgradeSystem.h

---
+++ Source/Upgrade/UpgradeSystem.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "UpgradeLevel.h"
+#include "UpgradeComponent.generated.h"
+
+USTRUCT(BlueprintType)
+struct FUpgradeData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Upgrade")
+    EUpgradeLevel Level = EUpgradeLevel::None;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Upgrade")
+    float BonusValue = 0.f;
+};
+
+UCLASS(Blueprintable, BlueprintType)
+class YOURGAME_API UUpgradeComponent : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    UUpgradeComponent();
+
+    /** Target Actor to upgrade */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Upgrade")
+    TWeakObjectPtr<AActor> TargetActor;
+
+    /** Upgrade data for levels */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Upgrade")
+    TMap<EUpgradeLevel, FUpgradeData> Upgrades;
+
+    /** Attempt to increase upgrade level on target actor */
+    UFUNCTION(BlueprintCallable, Category="Upgrade")
+    bool IncreaseLevel();
+
+    /** Attempt to decrease upgrade level on target actor */
+    UFUNCTION(BlueprintCallable, Category="Upgrade")
+    bool DecreaseLevel();
+
+    /** Get current upgrade data from target */
+    UFUNCTION(BlueprintCallable, Category="Upgrade")
+    FUpgradeData GetCurrentUpgradeData() const;
+
+protected:
+    /** Helper to get current level from target actor */
+    EUpgradeLevel GetTargetCurrentLevel() const;
+
+    /** Helper to set current level on target actor */
+    bool SetTargetCurrentLevel(EUpgradeLevel NewLevel);
+};
